cc147ffb7402c2c42233623aeac16ce08b170e60
kundan6378 <131573601+kundan6378@users.noreply.github.com>
2023-12-05 10:53:20 +0530

    Add files via upload

---
+++ sign-language-detector-python/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/run_script', methods=['POST'])
+def run_script():
+    subprocess.run(['python', 'inference_classifier.py'])
+    return 'Script executed successfully!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
